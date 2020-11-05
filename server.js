d951f8ffc9da6019b4ddb5f361dc4157cde810b9
claire p <cpuckett9@gmail.com>
2020-11-04 18:05:41 -0600

    server working

--- server.js
+++ server.js
@@ -6,9 +6,41 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//handlebars setup
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+//end handlebars setup
+
+//session setup
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+//end session setup
+
+
+//make styles and html available to server
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+// app.use(routes);
+
 
 
 // turn on connection to db and server
