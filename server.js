839663a970313f35c61c0b16efd0f9b847e58bfc
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-11-10 16:21:53 -0600

    update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const fileUpload = require('express-fileupload');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
