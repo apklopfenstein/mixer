7b33ce8ade3d91e594c5382c3e1b53995660e29a
claire p <cpuckett9@gmail.com>
2020-11-12 19:18:41 -0600

    homepage navbar fixed

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## Purpose
 The purpose of Mixr is to give musicians a project management system to organize songs or clips into a specific project.
 
+## Features
+Can create an account or log in, then  view your projects and/or create a new one. When clicking on a project, you are redirected to a page that contains songs associated with the project. can create songs with title, description, and mp3. Can delete songs within a project and whole projects.
+
 ## Screenshot
 <img src="./public/assets/images/screenshot.jpg" width="400px;">
 
--- public/javascript/newsong.js
+++ public/javascript/newsong.js
@@ -4,26 +4,6 @@ $("#back-btn").on("click", () => {
   $(location).attr("href", "/projects")
 })
 
-
-// //on new song button click, collect info and redirect to projects page
-// $("#newSong-btn").on("click", (event)=>{
-//     var title = $("#title").val().trim()
-//     var description = $("#description").val().trim()
-//     //attach mp3
-//     var song = $("#song").val()
-//     event.preventDefault();
-//     console.log(title, description, song);
-//     console.log("clicked");
-
-// //call function to add song to db with info as parameters
-// addSongDb(title, description, song)
-
-
-//     //redirect
-//     // $(location).attr("href", "/project-select")
-
-// })
-
 async function addSongDb(title, description) {
 
   console.log(title);
